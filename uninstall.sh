78d84eadd2583487635342547892c7cc6acaeff4
Maxim Belyayev <mbelyayev@noreply.codeberg.org>
2026-08-04 11:40:11 -0400

    remove brew uninstall docker since homebrew uninstall does so

--- uninstall.sh
+++ uninstall.sh
@@ -10,10 +10,6 @@ if command -v uv >/dev/null 2>&1; then
 fi
 echo 'uv uninstalled.'
 
-# Docker
-brew uninstall docker docker-compose
-echo 'Docker uninstalled.'
-
 # Homebrew
 if command -v brew >/dev/null 2>&1; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh | bash
