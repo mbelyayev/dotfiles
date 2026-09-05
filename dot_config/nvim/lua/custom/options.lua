a820b663915d33e664d7d87921ca7dfe6525f2ee
Maxim Belyayev <mbelyayev@noreply.codeberg.org>
2026-09-05 16:34:57 -0400

    add custom autocmd to set relative line nums for man file bufs in nvim

--- dot_config/nvim/lua/custom/options.lua
+++ dot_config/nvim/lua/custom/options.lua
@@ -1,7 +1,15 @@
 -- Don't wrap text; disorienting
 vim.o.wrap = false
 
-
 -- Gives floating/popup menu windows border and exposes highlight group customization.
 -- See `:help pumborder`. Setting winborder got me what I needed with colorscheme.lua.
 vim.o.winborder = 'rounded'
+
+-- Override settings set for man file types by
+-- opt/homebrew/Cellar/neovim/<version>/share/nvim/runtime/ftplugin/man.vim
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = "man",
+  callback = function()
+    vim.opt_local.relativenumber = true
+  end
+})
